8fcc4a1e0d22ab0d46310009d1e586ecac3068ae
Andrew <40874538+AndrewH-ML@users.noreply.github.com>
2025-11-04 20:55:11 -0800

    changes for face recognition model

--- neural_network/face_recognition/model/face_net.py
+++ neural_network/face_recognition/model/face_net.py
@@ -1,6 +1,49 @@
+import tensorflow as tf
 import tf_keras as keras 
 from keras import layers, models
 from keras.initializers import glorot_uniform 
 from keras import backend as K 
 K.set_image_data_format('channels_last')
+import os 
 
+
+from keras.models import model_from_json
+
+json_file = open('keras-facenet-h5/model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+model = model_from_json(loaded_model_json)
+model.load_weights('keras-facenet-h5/model.h5')
+
+print(model.inputs)
+print(model.outputs)
+
+# ignore below for now
+def triplet_loss(y_true, y_pred, alpha = 0.2):
+    """
+    Implementation of the triplet loss as defined by formula (3)
+    
+    Arguments:
+    y_true -- true labels, required when you define a loss in Keras, you don't need it in this function.
+    y_pred -- python list containing three objects:
+            anchor -- the encodings for the anchor images, of shape (None, 128)
+            positive -- the encodings for the positive images, of shape (None, 128)
+            negative -- the encodings for the negative images, of shape (None, 128)
+    
+    Returns:
+    loss -- real number, value of the loss
+    """
+    
+    anchor, positive, negative = y_pred[0], y_pred[1], y_pred[2]
+    
+
+    pos_dist = tf.square(anchor - positive)
+
+    neg_dist = tf.square(anchor - negative)
+
+    basic_loss = None
+
+    loss = None
+
+    
+    return loss
